472daf22716096236325dd55a806a735b3f36fda
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-03 01:37:02 -0600

    Wrote some initial sudo code

---
+++ dec2/part1.js
@@ -0,0 +1,16 @@
+// ! part 1 -
+//  sounds like I need an object for selected shapes and an object for round outcome
+// data needs odd inputs to be pushed to an array and even inputs pushed to an array
+// need switch statements for each possiblity
+// selected shapes isn't something that's "calulated" as opposed to round outcome
+// once round outcome is calculated selected shape needs to be added
+
+// selectedShapes = [
+    // {shape: 'rock', shapePoints: 1, winsOver: 'scissors', losesOver: 'paper', oppCode: 'A', secCol: 'X'}
+    // {shape: 'paper', shapePoints: 2, winsOver: 'rock', losesOver: 'scissors', oppCode: 'B', secCol: 'Y'}
+    // {shape: 'scissors', shapePoints: 3, winsOver: 'paper', losesOver: 'rock', oppCode: 'C', secCol: 'Z'}
+// ]
+
+// roundOutcome = [
+    // {winPoints: 6, drawPoints: 3, losePoints: 0,}
+// ]
