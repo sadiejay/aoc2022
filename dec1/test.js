154583e4e8b4b1498f72e6af4b92ad02f82e923b
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-03 01:10:01 -0600

    Uncomments test scripts

--- dec1/test.js
+++ dec1/test.js
@@ -1,12 +1,12 @@
-// import { getData, processData } from "./part1.js";
+import { getData, processData } from "./part1.js";
 import { getDatap2, processDatap2 } from "./part2.js";
 
-// // part 1 tests
-// getData('test.txt').then(data => {
-//     let result = processData(data);
-//     let expected = 24000;
-//     console.log("p1- test.txt",result === expected, {result,expected});
-// })
+// part 1 tests
+getData('test.txt').then(data => {
+    let result = processData(data);
+    let expected = 24000;
+    console.log("p1- test.txt",result === expected, {result,expected});
+})
 
 // part 2 tests
 getDatap2("test.txt").then((data) => {
