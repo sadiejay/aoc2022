937392b3abde81e63bd31a7cbf36d2ad1213b03e
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-03 02:16:53 -0600

    Adds import statements

--- dec2/test.js
+++ dec2/test.js
@@ -1,3 +1,5 @@
+import { getData, processData } from "./part1.js";
+// import { getDatap2, processDatap2 } from "./part2.js";
 getData("test.txt").then((data) => {
   let result = processData(data);
   let expected = 15;
