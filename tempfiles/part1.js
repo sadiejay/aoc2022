5702eae2b7496e3bf524428fa48aea6be1fab71d
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-04 15:47:47 -0600

    Creates template files

---
+++ tempfiles/part1.js
@@ -0,0 +1,30 @@
+// ! part 1 -
+
+
+// get Data
+export const getData = async (inputFile) => {
+    let rawInputs = await fetch(inputFile)
+        .then(response => response.text());
+    return rawInputs;
+};
+
+getData('test.txt')
+
+// write the code that processes data
+export const processData = (rawInputs) => {
+  // takes raw input and splits into new array
+  let inputs = [...(rawInputs.replace(/\n/g, " ").split(" "))];
+  console.log(`These are the inputs ${inputs}`);
+
+  // end processData
+};
+
+
+
+
+// spit out results
+getData("test.txt")
+  .then((data) => processData(data))
+  .then((lastVariable) =>
+    console.log(`🤔 The total score is ${lastVariable} points 👀`)
+  );
---
+++ tempfiles/readme.md
@@ -0,0 +1,15 @@
+# I'm working on it
+
+<br>
+<br>
+<br>
+
+## Notes:
+
+<br>
+<br>
+<br>
+
+
+
+## Resources used
