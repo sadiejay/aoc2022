a0d7da1c7bb4d7782cdb50c0150c59d38fb269a4
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-04 01:04:06 -0600

    Adds starting code for part2 and set for other files

--- dec2/test.js
+++ dec2/test.js
@@ -1,7 +1,17 @@
-import { getData, processData } from "./part1.js";
-// import { getDatap2, processDatap2 } from "./part2.js";
-getData("test.txt").then((data) => {
-  let result = processData(data);
-  let expected = 15;
-  console.log("p1- test.txt", result === expected, { result, expected });
+// import { getData, processData } from "./part1.js";
+import { getDatap2, processDatap2 } from "./part2.js";
+
+// part 1 tests
+// getData("test.txt").then((data) => {
+//   let result = processData(data);
+//   let expected = 15;
+//   console.log("p1- test.txt", result === expected, { result, expected });
+// });
+
+// part 2 tests
+getDatap2("test.txt").then((data) => {
+  let result = processDatap2(data);
+  let expected = 12;
+  console.log("p2- test.txt", result === expected, { result, expected });
 });
+
