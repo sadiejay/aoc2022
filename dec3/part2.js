5fb5f2da8ca36bf066bab10cc70f48bba0181207
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-04 16:02:31 -0600

    Adds folder for day3

---
+++ dec3/part2.js
@@ -0,0 +1,26 @@
+// ! part 2 -
+
+
+// get Data
+export const getDatap2 = async (inputFile) => {
+  let rawInputs = await fetch(inputFile).then((response) => response.text());
+  return rawInputs;
+};
+
+getDatap2("test.txt");
+
+// write the code that processes data
+export const processDatap2 = (rawInputs) => {
+  // takes raw input and splits into new array
+  let inputs = [...rawInputs.replace(/\n/g, " ").split(" ")];
+  console.log(`These are the inputs ${inputs}`);
+
+  // end processDatap2
+};
+
+// spit out results
+getDatap2("test.txt")
+  .then((data) => processDatap2(data))
+  .then((lastVariable) =>
+    console.log(`🤔 The total score is ${lastVariable} points 👀`)
+  );
