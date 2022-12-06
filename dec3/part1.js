0b1bb247cfbe141c8f54aea01fb5a3e4b2aaee85
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-05 19:36:14 -0600

    My js until I realized my strategy was wrong....

--- dec3/part1.js
+++ dec3/part1.js
@@ -26,8 +26,46 @@ getData('test.txt')
 // write the code that processes data
 export const processData = (rawInputs) => {
   // takes raw input and splits into new array
-  let inputs = [...(rawInputs.replace(/\n/g, " ").split(" "))];
+  let inputs = [...rawInputs.split(/\n/)];
+  // split each line and add to array (1 element in array = 1 rucksack)
   console.log(`These are the inputs ${inputs}`);
+  console.log(inputs);
+
+  // take each element and divide in half (one half = 1 compartment)
+  let compartment1 = "";
+  let compartment2 = "";
+  let allCompartmentsArr = [];
+  let compartment1Arr = [];
+  let compartment2Arr = [];
+  inputs.forEach((rucksack) => {
+    // take element(rucksack) and divide evenly
+    // str.slice(0, str.length / 2)
+    compartment1 = rucksack.slice(0, rucksack.length / 2);
+    compartment2 = rucksack.slice(rucksack.length / 2, rucksack.length);
+
+    // create compartment arrays
+    compartment1Arr.push(compartment1);
+    compartment2Arr.push(compartment2);
+
+    // create a pair with each compartment
+    let allCompartmentsArr = compartment1Arr.map(function (n, i) {
+      return  `${n}, ${compartment2Arr[i]}`;
+    });
+    console.log(allCompartmentsArr);
+    let priorityLetterArr = [];
+    let priorityLetter = '';
+
+    // i WANT to id duplicates
+
+
+
+
+  });
+
+  // !!! [NOOOO] remove all letters that are in common (use Set?) put in array
+
+  // find priority for all letters in array
+  // add letters in array
 
   // end processData
 };
