1147ddb9da19d39c8f6c23fb2622f69e966b9d68
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-12-05 19:35:36 -0600

    Preps for part1 inputs

--- dec3/test.js
+++ dec3/test.js
@@ -1,17 +1,17 @@
 import { getData, processData } from "./part1.js";
-import { getDatap2, processDatap2 } from "./part2.js";
+// import { getDatap2, processDatap2 } from "./part2.js";
 
 // part 1 tests
 getData("test.txt").then((data) => {
   let result = processData(data);
-  let expected = x;
+  let expected = 157;
   console.log("p1- test.txt", result === expected, { result, expected });
 });
 
 // part 2 tests
-getDatap2("test.txt").then((data) => {
-  let result = processDatap2(data);
-  let expected = x;
-  console.log("p2- test.txt", result === expected, { result, expected });
-});
+// getDatap2("test.txt").then((data) => {
+//   let result = processDatap2(data);
+//   let expected = x;
+//   console.log("p2- test.txt", result === expected, { result, expected });
+// });
 
